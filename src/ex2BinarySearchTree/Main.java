c0099581745a882315f6f52bbe14384426499e36
Tuan Viet Nguyen <lukasznguyen1999@gmail.com>
2020-05-30 12:19:24 +0200

    Modified BST and added build method

--- src/ex2BinarySearchTree/Main.java
+++ src/ex2BinarySearchTree/Main.java
@@ -4,42 +4,66 @@ package ex2BinarySearchTree;
 Implementation of Binary Search Tree
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        BinaryTree binTree = new BinaryTree();
-        /*                  9
-                           / \
-                          7   19
-                         /    /
-                        1    14
-                            / \
-                          12   18
-                           \   /
-                           13 16
-                               \
-                                17
+        Node root = new Node(12);
+        /*
+                                12
+                                /\
+                               5  15
+                              /\   /\
+                             3  7 13 17
+                            /    \     \
+                           1      9     19
          */
-        int [] values = {9, 19, 14, 7, 12, 18, 13, 16, 17, 1};
+        int[] values = {5, 15, 3, 7, 13, 17, 1, 9, 19};
+        List<Node> nodes = new ArrayList<>();
+        for (int value: values) {nodes.add(new Node(value)); }
+        //adding to tree
+        for(Node node : nodes) { root.insert(node); }
 
-        //Adding to tree
-        for (int value : values) {
-            binTree.insert(value);
-        }
-
-        //Printing tree
+        //printing tree
         System.out.print("Preorder: ");
-        BinaryTree.preorder(binTree.root);
+        root.preorder(root);
         System.out.println();
         System.out.print("Inorder: ");
-        BinaryTree.inorder(binTree.root);
+        root.inorder(root);
         System.out.println();
         System.out.print("Postorder: ");
-        BinaryTree.postorder(binTree.root);
+        root.postorder(root);
         System.out.println();
 
-        //Searching node
-        System.out.println(binTree.search(14)); //found
-        System.out.println(binTree.search(30)); //not found
+        //search node
+        //System.out.println(root.search(nodes.get(3))); //searching 7
+        //System.out.println(root.search(nodes.get(1))); //searching 15
+
+        //delete leaf(9)
+        //root.search(nodes.get(7)).delete(root.findParent(nodes.get(7)));
+        //System.out.println(root.search(nodes.get(3)));
+
+        //delete node with one child(7)
+        //root.search(nodes.get(3)).delete(root.findParent(nodes.get(3)));
+        //System.out.println(root.search(nodes.get(0)));
+
+        //delete node with one child(3)
+        //root.search(nodes.get(2)).delete(root.findParent(nodes.get(2)));
+        //System.out.println(root.search(nodes.get(0)));
+
+        //delete node with two children(5) - exception
+        //root.left.delete(root);
+        //System.out.println(root);
+
+        List<Integer> inorder = List.of(1,3,5,7,9,12,13,15,17,19);
+        List<Integer> postorder = List.of(1,3,9,7,5,13,19,17,15,12);
+        Node nroot = Node.build(inorder, postorder);
+        nroot.inorder(nroot);
+        System.out.println();
+        nroot.postorder(nroot);
+        System.out.println();
+        System.out.println(nroot);
 
     }
 }
--- src/ex2BinarySearchTree/Node.java
+++ src/ex2BinarySearchTree/Node.java
@@ -1,21 +1,144 @@
 package ex2BinarySearchTree;
 
+import java.util.List;
+
 public class Node {
 
     int value;
+    Node parent;
     Node left;
     Node right;
 
     public Node(int value) {
         this.value = value;
+        parent = null;
         left = null;
         right = null;
+    }
+
+    public Node(int value,Node parent, Node left, Node right){
+        this.value = value;
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public void insert(Node node){
+        if(node.getValue() < value){
+            if(left == null){
+                left = node;
+                left.parent = this;
+            } else {
+                left.insert(node);
+            }
+        } else {
+            if(right == null){
+                right = node;
+                right.parent = this;
+            } else {
+                right.insert(node);
+            }
+        }
+    }
+
+    public Node search(Node node){
+        if(node.getValue() == value){
+            return this;
+        }
+        if(node.getValue() < value){
+            return (left != null) ? left.search(node) : null;
+        }
+        return (right != null) ? right.search(node) : null;
+    }
+
+    public Node findParent(Node node){
+        if(left == node || right == node){
+            return this;
+        }
+
+        Node findLeft = (left != null) ? left.findParent(node) : null;
+
+        if(findLeft != null){
+            return findLeft;
+        }
+
+        return (right != null) ? right.findParent(node) : null;
+    }
+
+
+    public void remove(Node node){
+        if(node == left){
+            left = null;
+        }
+        if(node == right){
+            right = null;
+        }
+    }
+
+    public void delete(Node parent){
+        parent.remove(this);
+        if(left != null) {
+            parent.insert(left);
+        }
+        if(right != null) {
+            parent.insert(right);
+        }
+    }
+
+    // Parent - Left - Right
+    public void preorder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.getValue()+" ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    // Left - Parent - Right
+    public void inorder(Node node){
+        if(node == null){
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.getValue()+" ");
+        inorder(node.right);
+    }
+
+    // Left - Right - Parent
+    public void postorder(Node node){
+        if(node == null){
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.getValue()+" ");
+    }
+
+    public static Node build(List<Integer> in, List<Integer> post){
+        if(post.size()-1<0 || in.size()<1) return null;
+        int value = post.get(post.size()-1);
+        Node node = new Node(value);
+        List<Integer> lefti = in.subList(0, in.indexOf(value));
+        List<Integer> leftp = post.subList(0, in.indexOf(value));
+        List<Integer> righti = in.subList(in.indexOf(value)+1, in.size());
+        List<Integer> rightp = post.subList(in.indexOf(value), post.size()-1);
+        //node.left.parent = node;
+        node.left = build(lefti, leftp);
+        //node.right.parent = node;
+        node.right = build(righti, rightp);
+        return node;
     }
 
     @Override
     public String toString() {
         return "Node{" +
                 "value=" + value +
+                ", parent="+ parent+
                 ", left=" + left +
                 ", right=" + right +
                 '}';
